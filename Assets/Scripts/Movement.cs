0459e924a95cb9f07f2272dcdee79d066eb7097f
An21 <mesayahu@o2.pl>
2018-10-17 17:28:11 +0200

    Something is working

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public float runSpeed;
+    public float jumpSpeed;
+    public float jumpVel;
+    public float jumpDecaySpeed;
+    public float gravity;
+
+    public Rigidbody2D rig;
+
+    Vector2 dir = new Vector2(0, 0);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpVel = jumpSpeed;
+        }
+        if (jumpVel > 0)
+        {
+            jumpVel -= jumpDecaySpeed * jumpSpeed * Time.deltaTime;
+        }
+        dir = new Vector2(Input.GetAxis("Horizontal") * runSpeed * Time.deltaTime, jumpVel - gravity * Time.deltaTime);
+        rig.velocity = dir;
+    }
+}
