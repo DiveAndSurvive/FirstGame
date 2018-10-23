74eee00ff113d4b2aadd01f5228a048fa56fd4e1
michalszwarocki <44211267+michalszwarocki@users.noreply.github.com>
2018-10-23 02:07:44 +0200

    objects collecting and inventory add

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    public static Inventory instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public int inventorySpace = 16;
+    public List<Sprite> items = new List<Sprite>();
+
+    public bool Add(Sprite sprite)
+    {
+        if (items.Count >= inventorySpace)
+        {
+            return false;
+        }
+        items.Add(sprite);
+
+        return true;
+    }
+
+    public void Remove(Sprite sprite)
+    {
+        items.Remove(sprite);
+    }
+}
---
+++ Assets/Scripts/ObjectsCollecting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsCollecting : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            bool wasCollected = Inventory.instance.Add(other.gameObject.GetComponent<Sprite>());
+            if (wasCollected)
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
